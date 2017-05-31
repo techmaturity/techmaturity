46123796b8a214e7e9d3ec7515326566782e0643
Vignesh Jayabalan <Vignesh.Jayabalan@ticketmaster.com>
2017-05-31 13:40:07 -0700

    Tags Ui fixes, a bit of clean up

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
 	resources :products do
 		resources :scores, except: [:destroy, :update, :edit]
-		resources :tags
+		resources :tags, except: [:index, :show]
 	end
 
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
