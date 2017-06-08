8d112240303af3ca64f1a6b32a86a1b364cc1f6a
Jesse Emery <jesse@livenationlabs.com>
2017-06-07 21:05:03 -0400

    Add FactoryGirl and factories

--- Gemfile
+++ Gemfile
@@ -42,6 +42,10 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
+group :development, :test do
+  gem 'factory_girl_rails'
+end
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
---
+++ test/factories/product.rb
@@ -0,0 +1,30 @@
+require 'faker'
+
+product_types = ["Product", "Component", "Sub component"]
+
+FactoryGirl.define do
+
+  factory :product, class: Product do
+    name { Faker::Commerce.product_name }
+    product_type { product_types[Faker::Number.between(0, 2)] }
+    is_assessed false
+
+    factory :product_with_tags, class: Product do
+
+      transient do
+        tag_count { Faker::Number.between(1, 10) }
+      end
+
+      # the after(:create) yields two values; the user instance itself and the
+      # evaluator, which stores all values from the factory, including transient
+      # attributes; `create_list`'s second argument is the number of records
+      # to create and we make sure the user is associated properly to the post
+      after(:create) do |product, evaluator|
+        create_list(:tag, evaluator.tag_count, product: product)
+      end
+
+    end
+
+  end
+
+end
