0c5669822d03f97d6ea0b42cbaa218f4d11f25a4
David Igboanugo <david.igboanugo@ticketmaster.com>
2017-06-13 13:48:06 -0700

    Add tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -5,39 +5,39 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     @product = FactoryGirl.create(:product_with_tags)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get products_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_product_url
     assert_response :success
   end
 
-  test "should create product" do
+  test 'should create product' do
     assert_difference('Product.count') do
       post products_url, params: { product: { name: 'test product creation', product_type: 'test type' } }
     end
     assert_redirected_to product_url(Product.last)
   end
 
-  test "should show product" do
+  test 'should show product' do
     get product_url(@product)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_product_url(@product)
     assert_response :success
   end
 
-  test "should update product" do
+  test 'should update product' do
     patch product_url(@product), params: { product: { name: @product.name, product_type: @product.product_type } }
     assert_redirected_to product_url(@product)
   end
 
-  test "should soft delete product" do
+  test 'should soft delete product' do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
     end
@@ -45,5 +45,9 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_equal(Product.unscoped.count, (Product.count + 1))
 
     assert_redirected_to products_url
+  end
+
+  test 'should not assess not assessable products' do
+    visit product_url(@product)
   end
 end
