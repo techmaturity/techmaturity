b6707b13efbd4474bd9d4f654c8e8e83fec5b552
Jesse Emery <jesse@livenationlabs.com>
2017-06-14 14:55:55 -0400

    Fix test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,8 @@ require 'rails/test_help'
 require 'minitest/rails/capybara'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alclude FactoryGirl::Syntax::Methods
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+  include FactoryGirl::Syntax::Methods
   # Add more helper methods to be used by all tests here...
 end
