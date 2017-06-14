d583887671bdf92998565b4b8dcaeaf814bf9e32
Jesse Emery <jesse@livenationlabs.com>
2017-06-14 14:52:57 -0400

    Add simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,12 @@
+require "simplecov"
+SimpleCov.start
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/rails/capybara'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-  include FactoryGirl::Syntax::Methods
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alclude FactoryGirl::Syntax::Methods
   # Add more helper methods to be used by all tests here...
 end
